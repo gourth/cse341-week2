4b57795c87a92d4b93149df8f4165304416e1d85
Garrett Ourth <garrettourth@gmail.com>
2023-04-21 15:11:36 -0500

    add contact json to connect to mongodb

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ app
     res.setHeader('Access-Control-Allow-Origin', '*');
     next();
   })
-  .use('/professional', professionalRoutes);
+  // .use('/professional', professionalRoutes);
+  .use('/', require('./routes'));
 
 mongodb.initDb((err, mongodb) => {
   if (err) {
